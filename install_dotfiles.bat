6238a607e78a7d949c2881ea0adba548207bd4b0
takuyais <@takuyais>
2022-07-03 19:03:17 +0900

    Show installer commands

--- install_dotfiles.bat
+++ install_dotfiles.bat
@@ -1,6 +1,5 @@
-@echo off
-rem Need to run as Administrator
-setlocal
+@rem Need to run as Administrator
+@setlocal
 
 cd /d "%USERPROFILE%"
 mklink .bashrc dotfiles\bashrc
@@ -14,16 +13,16 @@ mkdir ctags.d\
 mklink ctags.d\config.ctags ..\dotfiles\config.ctags
 
 cd /d "%APPDATA%\Code\User"
-if not errorlevel 1 (
+@if not errorlevel 1 (
     mklink settings.json "%USERPROFILE%\dotfiles\settings.json"
     mklink keybindings.json "%USERPROFILE%\dotfiles\keybindings.json"
 )
 
-rem reg import NamingTemplates.reg
+@rem reg import NamingTemplates.reg
 
-rem Use SetEnvironmentVariable instead of SETX, to avoid side effects.
+@rem Use SetEnvironmentVariable instead of SETX, to avoid side effects.
 set GREP_OPTIONS=--exclude-dir=".svn" --exclude-dir=".git" --exclude-dir="node_modules" --exclude-dir="target" --exclude="*.class" --exclude "*.map"
-rem powershell -Command "[Environment]::SetEnvironmentVariable('GREP_OPTIONS', $env:GREP_OPTIONS, 'User')"
+@rem powershell -Command "[Environment]::SetEnvironmentVariable('GREP_OPTIONS', $env:GREP_OPTIONS, 'User')"
 
-endlocal
-pause
+@endlocal
+@pause
--- install_dotfiles_msys64.bat
+++ install_dotfiles_msys64.bat
@@ -1,23 +1,33 @@
-@echo off
-rem Need to run as Administrator
-setlocal
+@rem Need to run as Administrator
+@setlocal
 
 pushd "%SystemDrive%\msys64\home\%USERNAME%"
+
+@rem Bash
 mklink .bashrc_local "%USERPROFILE%\dotfiles\bashrc"
 mklink .aliases_tortoisegit "%USERPROFILE%\dotfiles\aliases_tortoisegit"
 mklink .gitconfig "%USERPROFILE%\dotfiles\gitconfig"
 mklink .gitconfig.local "%USERPROFILE%\dotfiles\gitconfig.local"
 mklink .gitignore_global "%USERPROFILE%\.gitignore_global"
+
+@rem Vim
 mklink .vimrc "%USERPROFILE%\dotfiles\vimrc"
 mklink .gvimrc "%USERPROFILE%\dotfiles\gvimrc"
-rem mklink /d vimfiles "%USERPROFILE%\vimfiles"
-xcopy /e "%USERPROFILE%\vimfiles" vimfiles\
+@rem mklink /d vimfiles "%USERPROFILE%\vimfiles"
+robocopy "%USERPROFILE%\vimfiles" vimfiles\ /e /xd swap
 mkdir vimfiles\swap
+
+@rem XDG Base Directory
+mkdir .local\bin
+mkdir .local\share
+
+@rem Etc.
 mklink .npmrc "%USERPROFILE%\.npmrc"
-xcopy /e "%USERPROFILE%\.aws" .aws\
+robocopy "%USERPROFILE%\.aws" .aws\ /e
+
 popd
 
 reg import open_mingw64_here.reg
 
-endlocal
-pause
+@endlocal
+@pause
