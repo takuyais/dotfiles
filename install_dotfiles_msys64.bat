c664a61e997889511b2728c015ba4c5bd86b1d83
takuyais <@takuyais>
2022-11-21 01:09:23 +0900

    Update install configs for msys2

--- install_dotfiles_msys64.bat
+++ install_dotfiles_msys64.bat
@@ -2,12 +2,15 @@
 @setlocal
 
 pushd "%SystemDrive%\msys64\home\%USERNAME%"
+if errorlevel 1 (
+  goto :exit
+)
 
 @rem Bash
 mklink .bashrc_local "%USERPROFILE%\dotfiles\bashrc"
 mklink .aliases_tortoisegit "%USERPROFILE%\dotfiles\aliases_tortoisegit"
 mklink .gitconfig "%USERPROFILE%\dotfiles\gitconfig"
-mklink .gitconfig.local "%USERPROFILE%\dotfiles\gitconfig.local"
+mklink .gitconfig.local "%USERPROFILE%\dotfiles\gitconfig.private-win"
 mklink .gitignore_global "%USERPROFILE%\.gitignore_global"
 
 @rem Vim
@@ -25,9 +28,19 @@ mkdir .local\share
 mklink .npmrc "%USERPROFILE%\.npmrc"
 robocopy "%USERPROFILE%\.aws" .aws\ /e
 
+reg import open_msys2_ucrt64_here.reg
+@rem reg import open_msys2_mingw64_here.reg
+
+:exit
 popd
-
-reg import open_mingw64_here.reg
-
 @endlocal
 @pause
+
+@rem TODO
+@rem  * Add following codes to .bashrc.
+@rem    source /usr/share/git/completion/git-prompt.sh
+@rem    source /usr/share/git/completion/git-completion.bash
+@rem    source "${HOME}/.bashrc_local"
+@rem  * Edit .bash_profile to add "${HOME}/.local/bin" to the PATH.
+@rem  * Install git-credential-manager.
+@rem    https://github.com/GitCredentialManager/git-credential-manager/issues/389
